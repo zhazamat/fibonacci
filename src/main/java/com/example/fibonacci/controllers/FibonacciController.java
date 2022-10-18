8861244401ddbe7f318f0f183faa6683caf2fb10
zhazamat <azamatkyzyzhanargul@gmail.com>
2022-10-18 16:50:49 +0600

    "Fibonacci"

--- src/main/java/com/example/fibonacci/controllers/FibonacciController.java
+++ src/main/java/com/example/fibonacci/controllers/FibonacciController.java
@@ -1,4 +1,45 @@
-package com.example.fibonacci;
+package com.example.fibonacci.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/v1/fibonacci")
+@RequiredArgsConstructor
 public class FibonacciController {
+
+    @GetMapping("/list")
+    public int [] getFibonacciNumbers(@RequestParam Integer n){
+        int[] arr = new int[n];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; ++i) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr;
+    }
+    @GetMapping("/recursion")
+    public int getFibonacciNumbersWithRecursion(@RequestParam Integer n){
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return getFibonacciNumbersWithRecursion(n - 1) + getFibonacciNumbersWithRecursion(n - 2);
+    }
+
+    @GetMapping("/iterator")
+    public int getFibonacciNumbersWithIterator(@RequestParam Integer n){
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        for (int i = 3; i <= n; i++) {
+            c = b + a;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
 }
+
+
